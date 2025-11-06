ac6aaf0309e7aa2739d9e5b633bb05c3760916e1
SebatianRD14 <sebasromo87@gmail.com>
2025-11-05 20:11:26 -0600

    feat: add android manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+// Dependencia para autenticación biométrica
+    implementation("androidx.biometric:biometric:1.1.0")
+// Para iconos adicionales
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
